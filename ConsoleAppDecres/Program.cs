abaf3f79b55c86f6b7c9f8ab079498eb6399ce4d
Usuario <Usuario@CPD02-M02>
2024-03-26 10:28:02 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppDecres/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDecres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 100;
+            while(cont > 0)
+            {
+                Console.WriteLine(cont);
+                cont--;
+            }
+            Console.ReadKey();
+        }
+    }
+}
